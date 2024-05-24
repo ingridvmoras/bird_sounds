9725d76cecbc6ffc510bfc33a299e9ac378dcbfe
Ingrid Vanessa Mora Sanchez <i.moras@uniandes.edu.co>
2024-05-24 13:41:25 -0500

    files download and processing

--- donwload_cds_data.r
+++ donwload_cds_data.r
@@ -1,5 +1,5 @@
 # Establecer el directorio de trabajo
-setwd("C:/Users/isabe/OneDrive/Documentos/GitHub/bird_sounds/files")
+setwd("./GitHub/bird_sounds/files")
 
 # Verificar e instalar paquetes necesarios
 if (!require(foreach) || !require(ecmwfr) || !require(googleway)) {
@@ -13,30 +13,31 @@ library(googleway)
 library(tidyverse)
 
 # Establecer la clave de usuario y clave de API para CDS
-wf_set_key(user ="308579", key = "4d274226-aa76-44b8-a235-013e93b4d869", service = "cds")
+wf_set_key(user ="inserte_su_usuario", key = "inserte_key", service = "cds")
 
 # Setear la clave de la API de Google Maps Elevation
-api_key <- "AIzaSyDTdfNAwyp4-5R-mgfhSOCwIF_dbvETlAw"
+api_key <- "inserte_key"
 
 # Leer los datos del archivo CSV
 datos <- read_delim("birdsongs_data.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
-d <- datos
-
 # Definir las variables a descargar
 variables <- c('2m_temperature', 'total_precipitation')
 
-# Iterar sobre cada fila de datos
-foreach(i = 1:nrow(d)) %do% {
+# Inicializar un data frame para almacenar los resultados
+resultados <- data.frame(ID = character(), fecha = character(), temperatura = numeric(), precipitacion = numeric(), altitud = numeric()) 
+
+# Inicializar un data frame para almacenar los resultados de altitud
+altitudes <- data.frame(ID = character(), altitud = numeric())
+
+# Iterar sobre cada fila de datos para obtener los datos de altitud
+foreach(i = 1:nrow(datos)) %do% {
   # Obtener los valores de la fila actual
-  nlat <- d[i, "nlat"]
-  slat <- d[i, "slat"]
-  wlon <- d[i, "wlon"]
-  elon <- d[i, "elon"]
-  anio <- as.numeric(d[i, "anio"])
-  region <- d[i, "region"]
-  fecha <- pull(d[i, "fecha"])
-  tx <- as.character(d[i, "ID"])
+  nlat <- datos[i, "nlat"]
+  slat <- datos[i, "slat"]
+  wlon <- datos[i, "wlon"]
+  elon <- datos[i, "elon"]
+  tx <- as.character(datos[i, "ID"])
   
   # Obtener datos de elevación utilizando Google Maps Elevation API
   elevation_data <- tryCatch(
@@ -51,9 +52,26 @@ foreach(i = 1:nrow(d)) %do% {
   if (!is.null(elevation_data)) {
     # Extraer la altitud del resultado
     altitud <- elevation_data$results$elevation
+    
+    # Agregar la altitud al dataframe de altitudes
+    altitudes <- bind_rows(altitudes, data.frame(ID = tx, altitud = altitud))
   } else {
-    altitud <- NA
+    # Si no se obtienen datos de altitud, asignar NA
+    altitudes <- bind_rows(altitudes, data.frame(ID = tx, altitud = NA))
   }
+}
+
+# Iterar sobre cada fila de datos para descargar y procesar los datos climáticos
+foreach(i = 1:nrow(datos)) %do% {
+  # Obtener los valores de la fila actual
+  nlat <- datos[i, "nlat"]
+  slat <- datos[i, "slat"]
+  wlon <- datos[i, "wlon"]
+  elon <- datos[i, "elon"]
+  anio <- as.numeric(datos[i, "anio"])
+  region <- datos[i, "region"]
+  fecha <- datos[i, "fecha"]
+  tx <- as.character(datos[i, "ID"])
   
   # Configurar la solicitud de descarga
   request <- list(
@@ -75,67 +93,65 @@ foreach(i = 1:nrow(d)) %do% {
       user = "308579",   # ID de usuario (para autenticación)
       request = request,  # la solicitud
       transfer = TRUE,     # descargar el archivo
-      path = "C:/Users/isabe/OneDrive/Documentos/GitHub/bird_sounds/files/era5data"       # almacenar los datos en el directorio de trabajo actual
+      path = "era5data"       # almacenar los datos en el directorio de trabajo actual
     ),
     error = function(e) {
       message("Error descargando datos para la fila ", i, ": ", conditionMessage(e))
       return(NULL)
     }
   )
-}
+  
+  # Listar todos los archivos .nc en la carpeta especificada
+  nc_files <- list.files(path = "./GitHub/bird_sounds/files/era5data", pattern = "\\.nc$", full.names = TRUE)
+  
+  # Inicializar un data frame para almacenar los resultados
+  resultados <- data.frame(ID = character(), fecha = character(), temperatura = numeric(), precipitacion = numeric()) 
+  
+  for (file in nc_files) {
+    # Verificar si se obtuvo el archivo correctamente
+    if (!is.null(file)) {
+      # Abrir el archivo NetCDF
+      nc <- nc_open(file)
+      
+      # Extraer las variables de interés
+      temp_kelvin <- ncvar_get(nc, "t2m")  # Temperatura en Kelvin
+      precipitacion <- ncvar_get(nc, "tp")  # Precipitación
+      
+      # Calcular la temperatura y precipitación promedio diaria
+      temp_promedio <- apply(temp_kelvin - 273.15, c(1, 2), mean)
+      temp_promedio_diaria <- mean(temp_promedio)
+      
+      precipitacion_total <- apply(precipitacion, c(1, 2), sum) * 1000  # Convertir a mm
+      precipitacion_diaria <- mean(precipitacion_total)
+      
+      # Cerrar el archivo NetCDF
+      nc_close(nc)
+      
+      # Obtener información del archivo para ID y fecha
+      filename <- basename(file)
+      info <- strsplit(filename, "_")
+      if (length(info[[1]]) >= 3) {
+        ID <- gsub(".nc", "", info[[1]][4])  # Suponiendo que el ID está en la cuarta posición del nombre del archivo
+        fecha <- substr(info[[1]][3], 1, 4)  # Extrayendo el año de la tercera posición del nombre del archivo
+      } else {
+        warning(paste("No se pudo extraer información de ID y fecha para el archivo:", filename))
+        next  # Saltar a la siguiente iteración del bucle
+      }
+      
+      # Agregar los resultados al data frame de resultados
+      resultados <- bind_rows(resultados, data.frame(
+        ID = ID,
+        fecha = fecha,
+        temperatura = temp_promedio_diaria,
+        precipitacion = precipitacion_diaria
+      ))
+    }
+  }
+  
+resultados <- merge(resultados, altitudes, by = "ID")
 
-nc_files <- list.files(path = "C:/Users/isabe/OneDrive/Documentos/GitHub/bird_sounds/files", pattern = "\\.nc$", full.names = TRUE)
-
-# Inicializar un data frame para almacenar los resultados
-resultados <- data.frame(ID = character(), fecha = character(), temperatura = numeric(), precipitacion = numeric(), altitud = numeric()) 
-
-# Iterar sobre cada archivo .nc
-for (file in nc_files) {
-  # Abrir el archivo NetCDF
-  nc <- nc_open(file)
-  
-  # Extraer información del archivo
-  anio <- as.numeric(substr(file, nchar(file)-12, nchar(file)-9))  # Extraer el año del nombre del archivo
-  tx <- substr(file, nchar(file)-7, nchar(file)-4)  # Extraer el ID del nombre del archivo
-  
-  # Extraer las variables de interés
-  temp_kelvin <- ncvar_get(nc, "t2m")  # Temperatura en Kelvin
-  
-  # Convertir la temperatura de Kelvin a Celsius
-  temp_promedio <- apply(temp_kelvin - 273.15, c(1, 2), mean)
-  temp_promedio_diaria <- mean(temp_promedio)
-  
-  # Extraer la precipitación total
-  precipitacion <- ncvar_get(nc, "tp")
-  
-  # Calcular la precipitación total
-  precipitacion_total <- apply(precipitacion, c(1, 2), sum) * 1000  # Convertir a mm
-  pre_total_diaria <- mean(precipitacion_total)
-  
-  # Extraer información de la fecha y el ID del archivo
-  # (puedes adaptar esto según la estructura de nombres de tus archivos .nc)
-  info <- strsplit(file, "_")
-  ID <- info[[1]][3]  # Suponiendo que el ID está en la tercera posición del nombre del archivo
-  fecha <- info[[1]][4]  # Suponiendo que la fecha está en la cuarta posición del nombre del archivo
-  
-  # Agregar los resultados al data frame
-  resultados <- bind_rows(resultados, data.frame(
-    ID = ID,
-    fecha = fecha,
-    temperatura = temp_promedio_diaria,
-    precipitacion = pre_total_diaria,
-    altitud = NA  # Puedes ajustar esto dependiendo de cómo obtienes la altitud en esta sección
-  ))
-  
-  # Cerrar el archivo NetCDF
-  nc_close(nc)
-  
-  # Eliminar el archivo NetCDF procesado
-  file.remove(file)
-}
-
-# Guardar los resultados en un archivo CSV
 write_csv(resultados, "climate_variables.csv")
 
 # Imprimir los resultados
 print(resultados)
+  
